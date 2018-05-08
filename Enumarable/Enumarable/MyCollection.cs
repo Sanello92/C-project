6f2e8fe7b574e93467ff1f2fdd19bdf890704b68
Sanello92 <sanyanesmiyanov92@gmail.com>
2018-05-08 22:13:53 +0300

    added project Enumerable

---
+++ Enumarable/Enumarable/MyCollection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TestEnumerable
+{
+
+    public class MyEnumerator : IEnumerator<int>
+    {
+
+        private int[] _array;
+
+        public MyEnumerator(int[] array)
+        {
+            _array = array;
+        }
+
+        private int _i = -1;
+
+        public int Current
+        {
+            get
+            {
+                try
+                {
+                    return _array[_i];
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("ouch");
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public void Dispose()
+        {
+            _array = null;
+        }
+
+        public bool MoveNext()
+        {
+            _i++;
+            return _i < _array.Length;
+        }
+
+        public void Reset()
+        {
+            _i = -1;
+        }
+    }
+
+    public class MyCollection : IEnumerable<int>
+
+    {
+        private int[] list;
+
+        public MyCollection(int[] elements)
+        {
+            list = elements;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new MyEnumerator(list);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Enumarable/Enumarable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new MyCollection(new int[4]{13, 123, 3, 1342});
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
