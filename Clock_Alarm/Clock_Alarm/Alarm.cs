12bb7bebaf09f9bdcc0109ad4e86a1907919edc3
Sanello92 <sanyanesmiyanov92@gmail.com>
2018-05-08 22:10:09 +0300

    add project Clock_Alarm

---
+++ Clock_Alarm/Clock_Alarm/Alarm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clock_Alarm
+
+{
+    public class Alarm
+    {
+        public event EventHandler<string> WakeUp;
+
+        
+
+        private int _hours;
+        private int _minutes;
+
+        public void SetAlarm(string input)
+        {
+            Parse(input);
+            WakeUp.Invoke(this, $"Time: {_hours} : {_minutes}");
+        }
+
+
+
+        private void Parse(string input)
+        {
+            TimeSpan time;
+            if (!TimeSpan.TryParse(input, out time))
+            {
+                throw new ValidationException("parsing error");
+            }
+
+            if (time.TotalDays > 0) // in case user input '24'
+            {
+                throw new ValidationException("parsing error");
+            }
+
+            _hours = time.Hours;
+            _minutes = time.Minutes;
+            
+        }
+
+
+    }
+}
---
+++ Clock_Alarm/Clock_Alarm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Clock_Alarm
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter time Alarm (hh:mm)");
+
+            string input = Console.ReadLine();
+
+            Alarm alarm = new Alarm();
+
+
+            alarm.WakeUp += Alarm_WakeUp;
+            alarm.SetAlarm(input);
+
+            
+            Console.ReadLine();
+
+        }
+
+        private static void Alarm_WakeUp(object sender, string e)
+        {
+            Alarm alarm = (Alarm) sender;
+            Console.WriteLine($"{e}");
+        }
+    }
+}
